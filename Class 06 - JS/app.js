2a045a86598272519b7e5ea341f146a42edda16e
Husnain Akram <husnainakram7860@gmail.com>
2025-09-29 20:45:51 +0800

    Class 06

---
+++ Class 06 - JS/app.js
@@ -0,0 +1,63 @@
+// alert("Hello world!")
+
+// document.write('hello world')
+// document.write('hello world')
+// document.write('hello world')
+// document.write('hello world')
+
+// var  a = "Hello World!"
+
+// document.write(a)
+// document.write(a)
+// document.write(a)
+// document.write(a)
+
+// var a = "hello" //string
+// var a = "20" //string
+// var a = 20 //number
+// var a = true/false //boolean
+// var a = ["hello",20,['a']] //array
+// var obj = {name:'Husnain', age:25} //obj
+
+// +
+// -
+// /
+// %
+// *
+// **
+
+// var a = 2
+// var b= 3
+// var c = a**b
+
+// document.write(c)
+
+// var a = 10 //string
+// var b = 10 //number
+
+// document.write(typeof a + "<br/>")
+// document.write(typeof b)
+
+// document.write(a<=b)
+
+// false and true
+// if (a != b) {
+//   document.write("a is equal to b");
+// } else {
+//   document.write("a is not equal to b");
+// }
+
+// var a = 15;
+// var b = 10;
+
+// if (a == b) {
+//   document.write("a is equal to b");
+// } else if (a < b) {
+//   document.write("a is less than b");
+// } else {
+//   console.log("a is greater b");
+// }
+
+// console.log("welcome icode guru")
+// console.warn("this is warning")
+// console.error("this is error")
